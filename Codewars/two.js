a7796aa74f5af54484ed5124b6286f1ecd048f75
Giselle bernal <giselle.bernal@adidas.com>
2023-01-11 17:46:13 -0500

    ejercicio2 codewards

---
+++ Codewars/two.js
@@ -0,0 +1,13 @@
+//? Sum of odd numbers
+
+const n = 2
+
+//Al ver el ejercio pense en Fibonacci o exponentes
+
+const rowSumOddNumbers = (n) => {
+    
+    return Math.pow(n, 3);
+
+}
+
+console.log(rowSumOddNumbers(n));
